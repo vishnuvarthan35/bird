62b541ca90eb54a92bf90b7571fe81034393e4b2
Vishnuvarthan Ramalingam <vramalingam@oeconnection.com>
2024-12-09 23:05:26 +0530

    first commit

---
+++ controllers/BirdController.js
@@ -0,0 +1,48 @@
+const birdService = require("../services/BirdService");
+var StatsD = require('hot-shots');
+var dogstatsd = new StatsD();
+
+exports.getAllBirds = async (req, res) => {
+  try {
+    const birds = await birdService.getAllBirds();
+    res.json({ data: birds, status: "success" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.createBird = async (req, res) => {
+  try {
+    const bird = await birdService.createBird(req.body);
+    res.json({ data: bird, status: "success" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.getBirdById = async (req, res) => {
+  try {
+    const bird = await birdService.getBirdById(req.params.id);
+    res.json({ data: bird, status: "success" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.updateBird = async (req, res) => {
+  try {
+    const bird = await birdService.updateBird(req.params.id, req.body);
+    res.json({ data: bird, status: "success" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.deleteBird = async (req, res) => {
+  try {
+    const bird = await birdService.deleteBird(req.params.id);
+    res.json({ data: bird, status: "success" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
